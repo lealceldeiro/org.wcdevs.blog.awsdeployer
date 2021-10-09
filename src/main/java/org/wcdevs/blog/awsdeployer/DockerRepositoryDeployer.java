f9c58b08db4a4f8c5131e94a914bf29c9fd7b1da
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-10-09 10:08:23 +0300

    Update DockerRepositoryDeployer
    - Clean code

--- src/main/java/org/wcdevs/blog/awsdeployer/DockerRepositoryDeployer.java
+++ src/main/java/org/wcdevs/blog/awsdeployer/DockerRepositoryDeployer.java
@@ -13,20 +13,20 @@ public class DockerRepositoryDeployer {
   private static final String NAME = "-docker-repository";
 
   public static void main(String[] args) {
-    App app = new App();
+    var app = new App();
 
     String accountId = getValueInApp("accountId", app);
     String region = getValueInApp("region", app);
     String applicationName = getValueInApp("applicationName", app);
 
-    StackProps stackProps = StackProps.builder()
-                                      .stackName(string(applicationName, NAME))
-                                      .env(environmentFrom(accountId, region))
-                                      .build();
-    Stack dockerRepositoryStack = new Stack(app, string(NAME, "Stack"), stackProps);
+    var stackProps = StackProps.builder()
+                               .stackName(string(applicationName, NAME))
+                               .env(environmentFrom(accountId, region))
+                               .build();
+    var dockerRepositoryStack = new Stack(app, string(NAME, "Stack"), stackProps);
 
-    DockerRepository.InputParameters inputParameters
-        = DockerRepository.newInputParameters(string(applicationName, NAME), accountId);
+    var inputParameters = DockerRepository.newInputParameters(string(applicationName, NAME),
+                                                              accountId);
     DockerRepository.newInstance(dockerRepositoryStack, NAME, inputParameters);
 
     app.synth();
