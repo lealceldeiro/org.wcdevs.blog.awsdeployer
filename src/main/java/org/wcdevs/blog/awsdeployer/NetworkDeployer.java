9d8ac7437d8968d98d05864e6ee00423b62eb635
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-11-16 14:20:22 +0200

    Fix: cast exception for some arguments

--- src/main/java/org/wcdevs/blog/awsdeployer/NetworkDeployer.java
+++ src/main/java/org/wcdevs/blog/awsdeployer/NetworkDeployer.java
@@ -19,8 +19,8 @@ public class NetworkDeployer {
     String accountId = Util.getValueInApp("accountId", app);
     String region = Util.getValueInApp("region", app);
     String sslCertificate = Util.getValueInApp("sslCertificate", app, false);
-    int internalPort = Util.getValueOrDefault("appInternalPort", app, 8080);
-    int externalPort = Util.getValueOrDefault("appExternalPort", app, 80);
+    var internalPort = Util.getValueOrDefault("appInternalPort", app, "8080");
+    var externalPort = Util.getValueOrDefault("appExternalPort", app, "80");
 
     var stackProps = StackProps.builder()
                                .stackName(Util.joinedString("-", environmentName, CONSTRUCT_NAME))
@@ -29,12 +29,22 @@ public class NetworkDeployer {
     var stack = new Stack(app, "NetworkStack", stackProps);
 
     var appEnv = new ApplicationEnvironment(applicationName, environmentName);
+    var listeningInternalPort = intOrDefault(internalPort, 8080);
+    var listeningExternalPort = intOrDefault(externalPort, 80);
     var inputParams = Network.InputParameters.builder()
-                                             .listeningInternalHttpPort(internalPort)
-                                             .listeningExternalHttpPort(externalPort)
+                                             .listeningInternalHttpPort(listeningInternalPort)
+                                             .listeningExternalHttpPort(listeningExternalPort)
                                              .sslCertificateArn(sslCertificate).build();
     Network.newInstance(stack, CONSTRUCT_NAME, appEnv, inputParams);
 
     app.synth();
+  }
+
+  private static int intOrDefault(String rawIntValue, int fallbackIntValue) {
+    try {
+      return Integer.parseInt(rawIntValue);
+    } catch (Exception e) {
+      return fallbackIntValue;
+    }
   }
 }
