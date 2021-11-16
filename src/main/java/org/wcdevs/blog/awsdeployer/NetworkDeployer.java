8a1d901b274dc7dc134f88819305c8a8d42207f0
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-11-16 12:28:45 +0200

    Update 02-create-shared-environment.yml and NetworkDeployer
    - Update GitFlow and NetworkDeployer to allow other parameters customization

--- src/main/java/org/wcdevs/blog/awsdeployer/NetworkDeployer.java
+++ src/main/java/org/wcdevs/blog/awsdeployer/NetworkDeployer.java
@@ -1,5 +1,6 @@
 package org.wcdevs.blog.awsdeployer;
 
+import org.wcdevs.blog.cdk.ApplicationEnvironment;
 import org.wcdevs.blog.cdk.Network;
 import org.wcdevs.blog.cdk.Util;
 import software.amazon.awscdk.core.App;
@@ -13,23 +14,26 @@ public class NetworkDeployer {
     var app = new App();
 
     String environmentName = Util.getValueInApp("environmentName", app);
+    var applicationName = Util.getValueOrDefault("applicationName", app,
+                                                 Network.DEFAULT_APPLICATION_NAME);
     String accountId = Util.getValueInApp("accountId", app);
     String region = Util.getValueInApp("region", app);
-    String applicationName = Util.getValueInApp("applicationName", app, false);
     String sslCertificate = Util.getValueInApp("sslCertificate", app, false);
     int internalPort = Util.getValueOrDefault("appInternalPort", app, 8080);
     int externalPort = Util.getValueOrDefault("appExternalPort", app, 80);
+
     var stackProps = StackProps.builder()
                                .stackName(Util.joinedString("-", environmentName, CONSTRUCT_NAME))
                                .env(Util.environmentFrom(accountId, region))
                                .build();
     var stack = new Stack(app, "NetworkStack", stackProps);
 
+    var appEnv = new ApplicationEnvironment(applicationName, environmentName);
     var inputParams = Network.InputParameters.builder()
                                              .listeningInternalHttpPort(internalPort)
                                              .listeningExternalHttpPort(externalPort)
                                              .sslCertificateArn(sslCertificate).build();
-    Network.newInstance(stack, CONSTRUCT_NAME, environmentName, /*applicationName,*/ inputParams);
+    Network.newInstance(stack, CONSTRUCT_NAME, appEnv, inputParams);
 
     app.synth();
   }
