4a496f4ebb3f91206a40307e4fe3fbb1ae377d0c
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-11-20 18:36:53 +0200

    Update CognitoDeployer
    - Add option for list of callback urls

--- src/main/java/org/wcdevs/blog/awsdeployer/CognitoDeployer.java
+++ src/main/java/org/wcdevs/blog/awsdeployer/CognitoDeployer.java
@@ -5,8 +5,15 @@ import org.wcdevs.blog.cdk.CognitoStack;
 import org.wcdevs.blog.cdk.Util;
 import software.amazon.awscdk.core.App;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class CognitoDeployer {
-  private static final String CONSTRUCT_NAME = "shared-cognito-stack";
+  private static final String CONSTRUCT_NAME = "cognito-stack";
+  private static final String DEFAULT_CALLBACKS = "";
 
   public static void main(String[] args) {
     var app = new App();
@@ -18,19 +25,32 @@ public class CognitoDeployer {
 
     String applicationUrl = Util.getValueInApp("applicationUrl", app);
     String domainPrefix = Util.getValueInApp("domainPrefix", app);
+    var callback = Util.getValueOrDefault("callback", app, DEFAULT_CALLBACKS);
 
     var awsEnvironment = Util.environmentFrom(accountId, region);
     var appEnv = new ApplicationEnvironment(applicationName, environmentName);
 
     var appDomainPrefix = Util.joinedString(Util.DASH_JOINER, environmentName, domainPrefix);
 
+    var callbackUrls = getCallbackUrls(callback);
     var input = CognitoStack.InputParameters.builder()
                                             .applicationUrl(applicationUrl)
                                             .loginPageDomainPrefix(appDomainPrefix)
+                                            .userPoolOauthCallBackUrls(callbackUrls)
                                             .build();
 
     CognitoStack.newInstance(app, CONSTRUCT_NAME, awsEnvironment, appEnv, input);
 
     app.synth();
+  }
+
+  private static List<String> getCallbackUrls(final String callback) {
+    return Arrays.stream(Optional.ofNullable(callback)
+                                 .orElse(DEFAULT_CALLBACKS)
+                                 .split(","))
+                 .filter(Objects::nonNull)
+                 .map(String::trim)
+                 .filter(s -> !s.isEmpty())
+                 .collect(Collectors.toList());
   }
 }
