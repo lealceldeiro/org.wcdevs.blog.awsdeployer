8765aee6df474a9dd0c0466a726220c1f56f9089
Asiel Leal Celdeiro <15990580+lealceldeiro@users.noreply.github.com>
2021-11-20 12:24:45 +0200

    Feature: Integrate cognito deployers (#25)
    * Update pom.xml
    - Bump cdk construct version
    
    * Create Cognito Deployer
    
    * Update core-app-deployment.yml
    - Retrieve cognito values from previously deployed cognito stack
    - Provide build with env vars for cognito integration
    
    * Update workflows
    
    * close #4

--- src/main/java/org/wcdevs/blog/awsdeployer/BEElasticContainerServiceDeployer.java
+++ src/main/java/org/wcdevs/blog/awsdeployer/BEElasticContainerServiceDeployer.java
@@ -1,6 +1,7 @@
 package org.wcdevs.blog.awsdeployer;
 
 import org.wcdevs.blog.cdk.ApplicationEnvironment;
+import org.wcdevs.blog.cdk.CognitoStack;
 import org.wcdevs.blog.cdk.Database;
 import org.wcdevs.blog.cdk.ElasticContainerService;
 import org.wcdevs.blog.cdk.Network;
@@ -18,31 +19,45 @@ import java.time.ZoneId;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.Map.entry;
+
 public class BEElasticContainerServiceDeployer {
   private static final String CONSTRUCT_NAME = "BEECServiceApp";
 
-  private static final String SPRING_PROFILES_ACTIVE = "SPRING_PROFILES_ACTIVE";
   private static final String CORE_APP_DB_URL = "CORE_APP_DB_URL";
   private static final String CORE_APP_DB_USER = "CORE_APP_DB_USER";
   private static final String CORE_APP_DB_PASSWORD = "CORE_APP_DB_PASSWORD";
   private static final String CORE_APP_DB_DRIVER = "CORE_APP_DB_DRIVER";
   private static final String CORE_APP_DB_DRIVER_POSTGRES = "org.postgresql.Driver";
+
   private static final String CORE_APP_LISTEN_PORT = "CORE_APP_LISTEN_PORT";
   private static final String CORE_APP_MANAGEMENT_PORT = "CORE_APP_MANAGEMENT_PORT";
+
   private static final String ENVIRONMENT_NAME = "ENVIRONMENT_NAME";
+  private static final String SPRING_PROFILES_ACTIVE = "SPRING_PROFILES_ACTIVE";
+
+  private static final String COGNITO_PROVIDER_URL = "COGNITO_PROVIDER_URL";
+  private static final String COGNITO_CLIENT_ID = "COGNITO_CLIENT_ID";
+  private static final String COGNITO_CLIENT_NAME = "COGNITO_CLIENT_NAME";
+  private static final String COGNITO_CLIENT_SECRET = "COGNITO_CLIENT_SECRET";
+  private static final String AWS_REGION = "AWS_REGION";
+
   private static final String SERVICE_STACK_NAME = "be-service-stack";
 
   public static void main(String[] args) {
     var app = new App();
 
-    String environmentName = Util.getValueInApp("environmentName", app);
     String accountId = Util.getValueInApp("accountId", app);
     String region = Util.getValueInApp("region", app);
     String applicationName = Util.getValueInApp("applicationName", app);
+
+    String environmentName = Util.getValueInApp("environmentName", app);
     var springProfile = Util.getValueOrDefault("springProfile", app, "aws");
+
     String dockerRepositoryName = Util.getValueInApp("dockerRepositoryName", app, false);
     String dockerImageTag = Util.getValueInApp("dockerImageTag", app, false);
     String dockerImageUrl = Util.getValueInApp("dockerImageUrl", app, false);
+
     var appListenPort = Util.getValueOrDefault("appPort", app, "8080");
     var appHealthCheckPath = Util.getValueOrDefault("healthCheckPath", app, "/");
     var appHealthCheckPort = Util.getValueOrDefault("healthCheckPort", app, "8080");
@@ -56,9 +71,11 @@ public class BEElasticContainerServiceDeployer {
     var dbOutputParameters = Database.outputParametersFrom(parametersStack,
                                                            applicationEnvironment);
 
-    var environmentVariables = environmentVariables(serviceStack, springProfile, environmentName,
-                                                    appListenPort, appHealthCheckPort,
-                                                    dbOutputParameters);
+    var cognitoParams = CognitoStack.getOutputParameters(serviceStack, applicationEnvironment);
+
+    var environmentVariables = environmentVariables(serviceStack, environmentName, springProfile,
+                                                    region, appListenPort, appHealthCheckPort,
+                                                    dbOutputParameters, cognitoParams);
     var secGroupIdsToGrantIngressFromEcs = secGroupIdAccessFromEcs(dbOutputParameters);
 
     var dockerImage = ElasticContainerService.newDockerImage(dockerRepositoryName, dockerImageTag,
@@ -102,20 +119,23 @@ public class BEElasticContainerServiceDeployer {
                                     Environment awsEnvironment) {
     var serviceStackName = applicationEnvironment.prefixed(SERVICE_STACK_NAME);
     return new Stack(app, "BEServiceStack", StackProps.builder()
-                                                    .stackName(serviceStackName)
-                                                    .env(awsEnvironment)
-                                                    .build());
+                                                      .stackName(serviceStackName)
+                                                      .env(awsEnvironment)
+                                                      .build());
   }
 
   private static List<String> secGroupIdAccessFromEcs(Database.OutputParameters dbOutput) {
     return List.of(dbOutput.getDbSecurityGroupId());
   }
 
-  private static Map<String, String> environmentVariables(Construct scope, String springProfile,
+  private static Map<String, String> environmentVariables(Construct scope,
+                                                          String awsRegion,
                                                           String environmentName,
+                                                          String springProfile,
                                                           String appListenPort,
                                                           String appHealthCheckPort,
-                                                          Database.OutputParameters dbOutput) {
+                                                          Database.OutputParameters dbOutput,
+                                                          CognitoStack.OutputParameters cogOutput) {
     var dbEndpointAddress = dbOutput.getEndpointAddress();
     var dbEndpointPort = dbOutput.getEndpointPort();
     var dbName = dbOutput.getDbName();
@@ -126,14 +146,27 @@ public class BEElasticContainerServiceDeployer {
     var dbUsername = dbSecret.secretValueFromJson(Database.USERNAME_SECRET_HOLDER).toString();
     var dbPassword = dbSecret.secretValueFromJson(Database.PASSWORD_SECRET_HOLDER).toString();
 
-    return Map.of(SPRING_PROFILES_ACTIVE, springProfile,
-                  CORE_APP_DB_URL, springDataSourceUrl,
-                  CORE_APP_DB_USER, dbUsername,
-                  CORE_APP_DB_PASSWORD, dbPassword,
-                  CORE_APP_DB_DRIVER, CORE_APP_DB_DRIVER_POSTGRES,
-                  CORE_APP_LISTEN_PORT, appListenPort,
-                  CORE_APP_MANAGEMENT_PORT, appHealthCheckPort,
-                  ENVIRONMENT_NAME, environmentName);
+    var cognitoClientSecret = CognitoStack.getUserPoolClientSecret(scope, cogOutput);
+    var cognitoClientSecretValue
+        = cognitoClientSecret.secretValueFromJson(CognitoStack.USER_POOL_CLIENT_SECRET_HOLDER)
+                             .toString();
+
+    return Map.ofEntries(entry(CORE_APP_DB_URL, springDataSourceUrl),
+                         entry(CORE_APP_DB_USER, dbUsername),
+                         entry(CORE_APP_DB_PASSWORD, dbPassword),
+                         entry(CORE_APP_DB_DRIVER, CORE_APP_DB_DRIVER_POSTGRES),
+
+                         entry(CORE_APP_LISTEN_PORT, appListenPort),
+                         entry(CORE_APP_MANAGEMENT_PORT, appHealthCheckPort),
+
+                         entry(ENVIRONMENT_NAME, environmentName),
+                         entry(SPRING_PROFILES_ACTIVE, springProfile),
+
+                         entry(COGNITO_PROVIDER_URL, cogOutput.getProviderUrl()),
+                         entry(COGNITO_CLIENT_ID, cogOutput.getUserPoolClientId()),
+                         entry(COGNITO_CLIENT_NAME, cogOutput.getUserPoolClientName()),
+                         entry(COGNITO_CLIENT_SECRET, cognitoClientSecretValue),
+                         entry(AWS_REGION, awsRegion));
   }
 
   private static ElasticContainerService.InputParameters inputParameters(
