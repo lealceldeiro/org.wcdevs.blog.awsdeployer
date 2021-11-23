85556ca68c9407803c9c7946e26349b2d2090a9d
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-11-23 14:46:21 +0200

    Create a deployment sequencer for each app and env
    - Update cdk-construct version
    - Update github workflow
    - Update DeploymentSequencerDeployer

--- src/main/java/org/wcdevs/blog/awsdeployer/DeploymentSequencerDeployer.java
+++ src/main/java/org/wcdevs/blog/awsdeployer/DeploymentSequencerDeployer.java
@@ -1,28 +1,29 @@
 package org.wcdevs.blog.awsdeployer;
 
+import org.wcdevs.blog.cdk.ApplicationEnvironment;
 import org.wcdevs.blog.cdk.DeploymentSequencerStack;
 import org.wcdevs.blog.cdk.Util;
 import software.amazon.awscdk.core.App;
 
 public class DeploymentSequencerDeployer {
-  private static final String CONSTRUCT_NAME = "DeploymentSequencerApp";
-
   public static void main(String[] args) {
     App app = new App();
 
+    String environmentName = Util.getValueInApp("environmentName", app);
     String applicationName = Util.getValueInApp("applicationName", app);
     String accountId = Util.getValueInApp("accountId", app);
     String region = Util.getValueInApp("region", app);
     String githubToken = Util.getValueInApp("githubToken", app);
-    var awsEnvironment = Util.environmentFrom(accountId, region);
 
+    var awsEnvironment = Util.environmentFrom(accountId, region);
+    var appEnv = new ApplicationEnvironment(applicationName, environmentName);
     var inputParams = DeploymentSequencerStack.InputParameters
         .builder()
         .codeDirectory("./deployment-sequencer-lambda/dist/lambda.zip")
         .githubToken(githubToken)
         .build();
-    DeploymentSequencerStack.newInstance(app, CONSTRUCT_NAME, awsEnvironment, applicationName,
-                                         inputParams);
+
+    DeploymentSequencerStack.newInstance(app, awsEnvironment, appEnv, inputParams);
 
     app.synth();
   }
